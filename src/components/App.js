0ccc8979169711f004c21a896dd6da94263b8d8c
Himanshu <raghav17498@gmail.com>
2023-01-27 23:28:20 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -8,16 +8,21 @@ class App extends React.Component{
   }
   constructor(props){
     super(props)
-    this.state = {count:0}
+    this.state = {count:0,even:0}
     this.handleClick = this.handleClick.bind(this)
   }
 
+  componentWillUpdate() {
+    if(this.state.count % 2 === 0){
+      this.setState({even:this.state.even = this.state.count})
+    }
+  }
 
   render(){
-    console.log(`Rendering with count:-${this.state.count}`)
+    console.log(`Rendering with count:-${this.state.even}`)
     return(
       <div>
-        <span id="count">{this.state.count}</span>
+        <span id="count">{this.state.even}</span>
         <button id="incr-btn" onClick={this.handleClick}>Increment</button>
       </div>
     )
